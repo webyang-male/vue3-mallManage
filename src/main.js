103cc3709c6587d69c93a4c4e0a5582097414f9a
webyang-male <mr.yangweb@gmail.com>
2022-06-30 21:51:46 +0800

    :pencil: 登录页响应式处理

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
-
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
 
 app.use(router)
 
 app.use(ElementPlus)
 
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
 import 'virtual:windi.css'
 
 app.mount('#app')
