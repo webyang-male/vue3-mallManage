218ebefe3af63144f3391ac40cd9a0c4d1f32053
webyang-male <mr.yangweb@gmail.com>
2022-12-10 14:31:18 +0800

    feat:keep alive page caching

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -1,15 +1,19 @@
 <template>
     <el-container>
         <el-header>
-            <f-header/>
+            <f-header />
         </el-header>
         <el-container>
             <el-aside :width="$store.state.asideWidth">
                 <f-menu></f-menu>
             </el-aside>
             <el-main>
-                <f-tag-list/>
-                <router-view></router-view>
+                <f-tag-list />
+                <router-view v-slot="{ Component }">
+                    <keep-alive :max="10">
+                        <component :is="Component"></component>
+                    </keep-alive>
+                </router-view>
             </el-main>
         </el-container>
     </el-container>
@@ -20,7 +24,7 @@ import FMenu from './components/FMenu.vue';
 import FTagList from './components/FTagList.vue';
 </script>
 <style>
-.el-aside{
+.el-aside {
     transition: all 0.2s;
 }
 </style>
